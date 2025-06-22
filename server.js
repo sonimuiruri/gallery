6cb7fe09a7ca4884a114e5bbe2a320d5bc44ffd3
winfrey_muthoni <muiruriwinfrey@gmail.com>
2025-06-23 00:03:15 +0300

    more changes

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/image', image);
 
 
  
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
